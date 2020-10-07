d928bf29846fb4838ade3e77661374dbe0c0c8c4
mark <mark@zeguestlist.com>
2020-10-07 14:00:46 +0800

    Add all files

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <appHeader></appHeader>
+      <Ninjas></Ninjas>
+    <appFooter></appFooter>
+  </div>
+</template>
+
+<script>
+
+import appHeader from "./components/header.vue";
+import appFooter from "./components/footer.vue";
+import Ninjas from "./components/ninjas.vue";
+
+export default {
+  components: {
+    appHeader, appFooter, Ninjas
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+    
+</style>
